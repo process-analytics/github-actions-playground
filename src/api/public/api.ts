d566f770349e9e2f8a6155cc2b671ea07fb3633e
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-04-06 16:11:50 +0200

    [DOC] Improve the API documentation used for testing

--- src/api/public/api.ts
+++ src/api/public/api.ts
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * Class to test the typedoc generation.
+ */
 export class TestAPI {
   /**
    * @internal
