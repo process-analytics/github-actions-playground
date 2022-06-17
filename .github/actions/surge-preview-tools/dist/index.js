c6533ae5d6bdb6a421f535603a1a428656129617
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-06-17 18:49:50 +0200

    Revert "update dist"
    This reverts commit 2f796d54c7848af1f695e71e20367269ecdf80b9.

--- .github/actions/surge-preview-tools/dist/index.js
+++ .github/actions/surge-preview-tools/dist/index.js
@@ -1,118 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 4822:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-// const core = require('@actions/core');
-// const github = require('@actions/github');
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __importDefault(__nccwpck_require__(5438));
-const surge_1 = __nccwpck_require__(983);
-try {
-    // default value
-    core_1.default.setOutput("should-run", true);
-    const payload = github_1.default.context.payload;
-    // Compute the 'preview url', as built by the surge-preview action
-    const repoOwner = github_1.default.context.repo.owner.replace(/\./g, '-');
-    const repoName = github_1.default.context.repo.repo.replace(/\./g, '-');
-    const url = `${repoOwner}-${repoName}-${github_1.default.context.job}-pr-${payload.number}.surge.sh`;
-    core_1.default.setOutput('preview-url', url);
-    core_1.default.info(`Computed preview url: ${url}`);
-    // the token must be set
-    const surgeToken = core_1.default.getInput('surge-token');
-    if (!surgeToken) {
-        core_1.default.setOutput("should-run", false);
-    }
-    // TODO on close PR, the deployment must exist
-    // if(payload.action === 'closed') {
-    else {
-        core_1.default.setSecret(surgeToken);
-        core_1.default.startGroup('List Surge domains');
-        const deploys = (0, surge_1.getDeploys)(surgeToken);
-        const domains = deploys.map(deploy => deploy.domain);
-        core_1.default.info(`Number of domains: ${domains.length}`);
-        domains.forEach(domain => core_1.default.debug(domain));
-        core_1.default.endGroup();
-        // TODO use includes
-        const isDomainExist = domains.indexOf(url) > 0;
-        core_1.default.info(`Domain exist? ${isDomainExist}`);
-        core_1.default.setOutput('domain-already-exist', isDomainExist);
-    }
-}
-catch (error) {
-    // @ts-ignore
-    core_1.default.setFailed(error === null || error === void 0 ? void 0 : error.message);
-}
-
-
-/***/ }),
-
-/***/ 983:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-// The following is adapted from https://github.com/adrianjost/actions-surge.sh-teardown/blob/fc7c144291330755517b28a873139fcc11327cd8/index.js#L17
-// released under the MIT license
-// TODO move to a dedicated file
-// TODO use stripAnsi 7 move to ESM https://github.com/chalk/strip-ansi/releases/tag/v7.0.0
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.getDeploys = void 0;
-const strip_ansi_1 = __importDefault(__nccwpck_require__(8770));
-const child_process_1 = __nccwpck_require__(2081);
-// import execSync from "child_process";
-// const { exec } = require("child_process");
-// const { execSync } = require("child_process");
-function executeCmd(command) {
-    // return new Promise((resolve, reject) => {
-    //   exec(command, function(error, stdout) {
-    //     error
-    //       ? reject(error)
-    //       : resolve(stripAnsi(stdout).trim());
-    //   });
-    // });
-    const result = (0, child_process_1.execSync)(command);
-    return (0, strip_ansi_1.default)(result.trim());
-    // return stripAnsi(result.stdout).trim()
-}
-// Adapted here to pass the surge token
-function getDeploys(surgeToken) {
-    const surgeListOutput = executeCmd(`npx surge list --token ${surgeToken}`);
-    const lines = (0, strip_ansi_1.default)(surgeListOutput)
-        .trim()
-        .split("\n")
-        .map(l => l.trim().replace(/ {3,}/g, "  "));
-    return lines.map(line => {
-        const deploy = line.split("  ").map(a => a.trim());
-        const [id, domain] = deploy[0].split(" ");
-        const [, timestamp, provider, host, plan] = deploy;
-        return {
-            id,
-            domain,
-            timestamp,
-            provider,
-            host,
-            plan,
-            line
-        };
-    });
-}
-exports.getDeploys = getDeploys;
-// END of surge utils code
-
-
-/***/ }),
-
 /***/ 7351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -4421,6 +4309,24 @@ exports.request = request;
 
 /***/ }),
 
+/***/ 5063:
+/***/ ((module) => {
+
+"use strict";
+
+
+module.exports = ({onlyFirst = false} = {}) => {
+	const pattern = [
+		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
+	].join('|');
+
+	return new RegExp(pattern, onlyFirst ? undefined : 'g');
+};
+
+
+/***/ }),
+
 /***/ 3682:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -6422,6 +6328,18 @@ function onceStrict (fn) {
   f.called = false
   return f
 }
+
+
+/***/ }),
+
+/***/ 5591:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+"use strict";
+
+const ansiRegex = __nccwpck_require__(5063);
+
+module.exports = string => typeof string === 'string' ? string.replace(ansiRegex(), '') : string;
 
 
 /***/ }),
@@ -9062,42 +8980,6 @@ module.exports = require("zlib");
 
 /***/ }),
 
-/***/ 8770:
-/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __nccwpck_require__) => {
-
-"use strict";
-// ESM COMPAT FLAG
-__nccwpck_require__.r(__webpack_exports__);
-
-// EXPORTS
-__nccwpck_require__.d(__webpack_exports__, {
-  "default": () => (/* binding */ stripAnsi)
-});
-
-;// CONCATENATED MODULE: ./node_modules/ansi-regex/index.js
-function ansiRegex({onlyFirst = false} = {}) {
-	const pattern = [
-	    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
-	].join('|');
-
-	return new RegExp(pattern, onlyFirst ? undefined : 'g');
-}
-
-;// CONCATENATED MODULE: ./node_modules/strip-ansi/index.js
-
-
-function stripAnsi(string) {
-	if (typeof string !== 'string') {
-		throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
-	}
-
-	return string.replace(ansiRegex(), '');
-}
-
-
-/***/ }),
-
 /***/ 2020:
 /***/ ((module) => {
 
@@ -9139,45 +9021,101 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(4822);
-/******/ 	module.exports = __webpack_exports__;
-/******/ 	
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(2186);
+const github = __nccwpck_require__(5438);
+
+// The following is adapted from https://github.com/adrianjost/actions-surge.sh-teardown/blob/fc7c144291330755517b28a873139fcc11327cd8/index.js#L17
+// released under the MIT license
+// TODO move to a dedicated file
+// TODO use stripAnsi 7 move to ESM https://github.com/chalk/strip-ansi/releases/tag/v7.0.0
+const stripAnsi = __nccwpck_require__(5591);
+// const { exec } = require("child_process");
+const { execSync } = __nccwpck_require__(2081);
+
+function executeCmd(command) {
+  // return new Promise((resolve, reject) => {
+  //   exec(command, function(error, stdout) {
+  //     error
+  //       ? reject(error)
+  //       : resolve(stripAnsi(stdout).trim());
+  //   });
+  // });
+  const result = execSync(command);
+  return stripAnsi(result.stdout).trim()
+}
+
+// Adapted here to pass the surge token
+function getDeploys(surgeToken) {
+  const surgeListOutput = executeCmd(`npx surge list --token ${surgeToken}`);
+  const lines = stripAnsi(surgeListOutput)
+    .trim()
+    .split("\n")
+    .map(l => l.trim().replace(/ {3,}/g, "  "));
+  return lines.map(line => {
+    const deploy = line.split("  ").map(a => a.trim());
+    const [id, domain] = deploy[0].split(" ");
+    const [, timestamp, provider, host, plan] = deploy;
+    return {
+      id,
+      domain,
+      timestamp,
+      provider,
+      host,
+      plan,
+      line
+    };
+  });
+}
+// END of surge utils code
+
+
+try {
+  // default value
+  core.setOutput("should-run", true);
+
+  const payload = github.context.payload;
+  // Compute the 'preview url', as built by the surge-preview action
+  const repoOwner = github.context.repo.owner.replace(/\./g, '-');
+  const repoName = github.context.repo.repo.replace(/\./g, '-');
+  const url = `${repoOwner}-${repoName}-${github.context.job}-pr-${payload.number}.surge.sh`;
+  core.setOutput('preview-url', url);
+  core.info(`Computed preview url: ${url}`);
+
+  // the token must be set
+  const surgeToken = core.getInput('surge-token');
+  if (!surgeToken) {
+    core.setOutput("should-run", false);
+  }
+  // TODO on close PR, the deployment must exist
+  // if(payload.action === 'closed') {
+  else {
+    core.setSecret(surgeToken);
+    core.startGroup('List Surge domains');
+    const deploys = getDeploys(surgeToken);
+    const domains = deploys.map(deploy => deploy.domain);
+    core.info(`Number of domains: ${domains.length}`);
+    core.debug(domains);
+    core.endGroup();
+
+    const isDomainExist = domains.find(domain => domain === url);
+    core.info(`Domain exist? ${isDomainExist}`);
+    core.setOutput('domain-already-exist', isDomainExist);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+
+})();
+
+module.exports = __webpack_exports__;
 /******/ })()
 ;
