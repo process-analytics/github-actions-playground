a516daa30112ef8cfaa7a163bfdce0875b339e17
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-06-20 07:29:04 +0200

    Revert "move source to ESM"
    This reverts commit 01a1ca4ba1e5d702a02173633f4496080a19323e.

--- .github/actions/surge-preview-tools/src/index.js
+++ .github/actions/surge-preview-tools/src/index.js
@@ -1,6 +1,5 @@
-import core from "@actions/core";
-import github from "@actions/github";
-
+const core = require('@actions/core');
+const github = require('@actions/github');
 const {checkLogin, getDeploys} = require("./surge-utils");
 
 try {
--- .github/actions/surge-preview-tools/src/surge-utils.js
+++ .github/actions/surge-preview-tools/src/surge-utils.js
@@ -1,7 +1,8 @@
 // The following is adapted from https://github.com/adrianjost/actions-surge.sh-teardown/blob/fc7c144291330755517b28a873139fcc11327cd8/index.js#L17
 // released under the MIT license
-import stripAnsi from "strip-ansi";
-import {execSync} from 'node:child_process'
+// TODO use stripAnsi 7 move to ESM https://github.com/chalk/strip-ansi/releases/tag/v7.0.0
+const stripAnsi = require("strip-ansi");
+const { execSync } = require("child_process");
 
 const surgeCli = 'npx surge';
 
