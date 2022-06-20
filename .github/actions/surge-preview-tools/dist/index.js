50e98543f0451674da18097610325861b4b3b4c1
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-06-20 06:31:04 +0200

    update dist

--- .github/actions/surge-preview-tools/dist/index.js
+++ .github/actions/surge-preview-tools/dist/index.js
@@ -8870,11 +8870,9 @@ function executeCmd(command) {
 
 const checkLogin = (surgeToken) => {
   try {
-  const surgeLoginOutput = executeCmd(`${surgeCli} list --token ${surgeToken}`);
-  console.info('surge login:', surgeLoginOutput);
+  executeCmd(`${surgeCli} list --token ${surgeToken}`);
   return true;
   } catch (e) {
-    console.info(e.message);
     return false;
   }
 }
